586d46d37bba194e9647abebac3bc376b739fa90
Nutpupicha Arungornpasuruk <6510615096@cn351>
2025-03-02 19:05:42 +0700

    home page front-end

--- solarsystem/settings.py
+++ solarsystem/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'solarsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
