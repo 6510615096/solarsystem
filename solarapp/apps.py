2d28de352e0b97e6181db4e5dd31d0f0e4dab758
Ponthipa Teerapravet <mild.ponthipa@gmail.com>
2025-05-31 16:35:48 +0700

    automatically add default roles

--- solarapp/apps.py
+++ solarapp/apps.py
@@ -6,3 +6,18 @@ class SolarappConfig(AppConfig):
 
     def ready(self):
         import solarapp.signals  
+        from django.db.utils import OperationalError
+        from .models import Role
+
+        try:
+            ROLE_CHOICES = [
+                ('Admin', 'Admin'),
+                ('Drone controller', 'Drone controller'),
+                ('Admin solar', 'Admin solar'),
+                ('Data analyst', 'Data analyst'),
+            ]
+            for role_name, _ in ROLE_CHOICES:
+                Role.objects.get_or_create(name=role_name)
+        except OperationalError:
+            pass
+
--- solarapp/models.py
+++ solarapp/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Role(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Drone controller', 'Drone controller'),
+        ('Admin solar', 'Admin solar'),
+        ('Data analyst', 'Data analyst'),
+    ]
+    name = models.CharField(max_length=50, unique=True, choices=ROLE_CHOICES)
     description = models.TextField(blank=True)
     
     def __str__(self):
         return self.name
+
+
     
 class SolarPlant(models.Model):
     name = models.CharField(max_length=255)
@@ -48,4 +56,4 @@ class UploadedFile(models.Model):
 
     def __str__(self):
         return f"{self.file.name} → {self.plant.name}"
-    
+
